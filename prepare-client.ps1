f36d840a298af5c4653c57e6726b9e58e0fbe3bf
beejayone <beejayone@gmail.com>
2025-01-13 15:52:58 +0100

    Create prepare-client.ps1

---
+++ prepare-client.ps1
@@ -0,0 +1,3 @@
+#prepare client script
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/beejayone/BritaSE-OSD/refs/heads/main/BritaSE_Lockscreen_3440x1935.jpg" -OutFile "C:\Windows\Web\Wallpaper\BritaSE_Lockscreen_3440x1935.jpg"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/beejayone/BritaSE-OSD/refs/heads/main/BritaSE_Wallpaper_3440x1935.jpg" -OutFile "C:\Windows\Web\Wallpaper\BritaSE_Wallpaper_3440x1935.jpg"
